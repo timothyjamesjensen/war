4a6609934fdf95c76c479bb57557b4e1d7bf90ff
= <timothyjamesjensen@gmail.com>
2016-04-19 16:03:52 -0700

    Created failing tests for Card class

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,17 @@
-public class CardTest {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+
+public class CardTest {
+    Card card = new Card("Hearts", "Queen");
+
+    @Test
+    public void SuitShouldEqualHearts() {
+        assertEquals("Suit must equal 'Hearts'", "Hearts", card.getSuit());
+    }
+
+    @Test
+    public void RankShouldEqualQueen() {
+        assertEquals("Rank must equal 'Queen'", "Queen", card.getRank());
+    }
 }
