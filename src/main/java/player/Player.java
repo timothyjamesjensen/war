06a90f46e710a66094f2d4516e20a9f0ad23d9c4
= <timothyjamesjensen@gmail.com>
2016-04-19 21:26:30 -0700

    created Player class

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,12 @@
+package player;
+
+import card.Card;
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> hand;
+
+    Player() {
+        this.hand = new ArrayList<>();
+    }
+}
---
+++ src/test/java/playertest/PlayerTest.java
@@ -0,0 +1,4 @@
+package playertest;
+
+public class PlayerTest {
+}
