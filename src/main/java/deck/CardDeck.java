bec7525f7a3235a503182651053b9299353f3cd7
= <timothyjamesjensen@gmail.com>
2016-04-19 19:41:48 -0700

    created ranks and suits arrays

--- src/main/java/deck/CardDeck.java
+++ src/main/java/deck/CardDeck.java
@@ -3,15 +3,19 @@ package deck;
 import card.Card;
 import static card.Rank.*;
 import static card.Suit.*;;
+import card.Rank;
 import card.Suit;
 
 public class CardDeck implements Deck {
 
     private int numberOfSuits;
     private int getNumberOfRanks;
+    private final Rank[] ranks;
+    private final Suit[] suits;
 
     public CardDeck() {
-
+        ranks = new Rank[]{TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE};
+        suits = new Suit[]{HEARTS,DIAMONDS,SPADES,CLUBS};
     }
 
     public void create(int numberOfSuits, int numberOfRanks) {
