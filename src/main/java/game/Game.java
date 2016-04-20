38828002547a504bfd904b2755358e6ce47d28c4
= <timothyjamesjensen@gmail.com>
2016-04-19 23:43:07 -0700

    created game loop

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -6,10 +6,12 @@ import player.Player;
 import java.util.ArrayList;
 
 public class Game {
-    ArrayList<Player> players;
+    private ArrayList<Player> players;
+    private final boolean winner;
 
     public Game() {
         players = new ArrayList<>();
+        this.winner = false;
     }
 
     public void play(int numberOfSuits, int numberOfRanks, int playersCount) {
@@ -18,9 +20,18 @@ public class Game {
         cards.create(numberOfSuits, numberOfRanks);
         cards.shuffle();
         dealCardsToPlayers(cards, playersCount);
+        gameLoop();
+    }
+
+    public void gameLoop() {
+        // this will loop until someone becomes a winner and winner is set to true
+        while (!winner) {
+
+        }
     }
 
     public void dealCardsToPlayers(Deck cards, int playersCount) {
+        // If there are 5 players, each player will get 10 cards and two will be left in the deck
         while (cards.getDeck().size() > 0  && (cards.getDeck().size() - playersCount) >= 0) {
             for (Player player : players) {
                 player.addCardsToHand(cards.deal());
