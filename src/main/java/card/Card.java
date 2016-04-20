b0b4112edf22c4d61d1f1eb15215593a780ebf2f
= <timothyjamesjensen@gmail.com>
2016-04-19 19:15:39 -0700

    fixing github failure

---
+++ src/main/java/card/Card.java
@@ -0,0 +1,18 @@
+package card;
+
+public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit; this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+}
---
+++ src/main/java/card/Rank.java
@@ -0,0 +1,33 @@
+package card;
+
+public enum Rank {
+    TWO(2, "Two"),
+    THREE(3, "Three"),
+    FOUR(4, "Four"),
+    FIVE(5, "Five"),
+    SIX(6, "Six"),
+    SEVEN(7, "Seven"),
+    EIGHT(8, "Eight"),
+    NINE(9, "Nine"),
+    TEN(10, "Ten"),
+    JACK(11, "Jack"),
+    QUEEN(12, "Queen"),
+    KING(13, "King"),
+    ACE(14, "Ace");
+
+    private final int value;
+    private final String name;
+
+    Rank(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
