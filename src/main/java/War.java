d261a989ed0046b9b038c03993147fa08eaad2a5
= <timothyjamesjensen@gmail.com>
2016-04-19 21:29:52 -0700

    added playerID field to player class

--- src/main/java/War.java
+++ src/main/java/War.java
@@ -1,7 +1,7 @@
 public class War {
     
     public static void main(String[] args) {
-
+        
     }
 
     public static void play( int numberOfSuits, int numberOfRanks, int numberOfPlayers ){
--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -4,9 +4,11 @@ import card.Card;
 import java.util.ArrayList;
 
 public class Player {
+    private final String playerID;
     private ArrayList<Card> hand;
 
-    Player() {
+    Player(String playerID) {
+        this.playerID = playerID;
         this.hand = new ArrayList<>();
     }
 }
