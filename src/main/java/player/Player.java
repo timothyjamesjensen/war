b3cc455df155668606862a0260d29ba08be53f15
= <timothyjamesjensen@gmail.com>
2016-04-19 21:47:04 -0700

    basic getters and setters for Player class completed

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,14 +1,32 @@
 package player;
 
 import card.Card;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Player {
     private final String playerID;
     private ArrayList<Card> hand;
 
-    Player(String playerID) {
+    public Player(String playerID) {
         this.playerID = playerID;
         this.hand = new ArrayList<>();
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addCardsToHand(Card card) {
+        hand.add(card);
+    }
+
+    public void addCardsToHand(ArrayList<Card> cards) {
+        hand.addAll(cards);
     }
 }
--- src/test/java/playertest/PlayerTest.java
+++ src/test/java/playertest/PlayerTest.java
@@ -1,4 +1,40 @@
 package playertest;
 
+import card.Card;
+
+import org.junit.Test;
+import player.Player;
+
+import java.util.ArrayList;
+
+import static card.Rank.*;
+import static card.Suit.*;
+import static org.junit.Assert.assertEquals;
+
 public class PlayerTest {
+    @Test
+    public void GetPlayerIDShouldReturnCorrectValue() {
+        Player player1 = new Player("player1");
+        assertEquals("PlayerID should equal player1", "player1", player1.getPlayerID());
+    }
+
+    @Test
+    public void GetPlayerHandShouldReturnThePlayersHand() {
+        Player player1 = new Player("player1");
+        Card newCard = new Card(SPADES, ACE);
+        player1.addCardsToHand(newCard);
+        assertEquals("Size of players hand should equal 1", 1, player1.getHand().size());
+    }
+
+    @Test
+    public void AddingMultipleCardsToHandShouldWork() {
+        Player player1 = new Player("player1");
+        Card newCard1 = new Card(SPADES, TWO);
+        Card newCard2 = new Card(SPADES, THREE);
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(newCard1);
+        cards.add(newCard2);
+        player1.addCardsToHand(cards);
+        assertEquals("size of players hand should equal 2", 2, player1.getHand().size());
+    }
 }
