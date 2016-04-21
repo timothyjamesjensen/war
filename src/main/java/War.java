7c806476546aa6ec040a0c58d16cfcda2e675ef8
= <timothyjamesjensen@gmail.com>
2016-04-20 22:12:14 -0700

    updated welcome

--- src/main/java/War.java
+++ src/main/java/War.java
@@ -4,6 +4,8 @@ import java.util.InputMismatchException;
 
 public class War {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         welcome();
     }
@@ -16,34 +18,37 @@ public class War {
     }
 
     public static void welcome() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the card game War!\n");
         System.out.println("Main Menu\n\nNew Game:  Any Letter+ENTER\nQuit:      q+ENTER");
-
         String input = scanner.next();
 
         if (input.equals("q")) {
             System.out.println("\n\nThanks for playing!");
         } else {
             System.out.println("\nHey! I'm glad you decided to play a game!!" +
-                    "I need you to enter how many players there will be and then" +
+                    "I need you a little bit of input from you and then" +
                     " we can get started!\n");
 
-            int playerCount = 0;
-            while (playerCount < 2 || playerCount > 5) {
-                try {
-                    System.out.print("Enter a Player count between 2 and 5: ");
-                    playerCount = scanner.nextInt();
-                    if (playerCount < 2 || playerCount > 5) {
-                        System.err.println("\nHey! I thought I said to enter a number between 2 and 5!\nTry again!");
-                        //scanner.next();
-                    }
-                } catch (InputMismatchException e) {
-                    System.err.println("\nHey! I thought I said to enter a NUMBER between 2 and 5!\nTry again!");
-                    scanner.next();
-                }
-            }
-            play(4, 13, playerCount);
+            play(getValidIntegerInput(2,4,"number of card suits"), getValidIntegerInput(6,13,"number or card ranks"),
+                    getValidIntegerInput(2,5,"Player Count"));
         }
+    }
+
+    public static int getValidIntegerInput(int floor, int ceiling, String inputName) {
+        int input = 0;
+        while (input < floor || input > ceiling) {
+            try {
+                System.out.print("Enter a " + inputName + " between " + floor + " and " + ceiling + ": ");
+                input = scanner.nextInt();
+                if (input < floor || input > ceiling) {
+                    System.out.println("\nHey! I thought I said to enter a number between 2 and 5!\nTry again!");
+                    //scanner.next();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nHey! I thought I said to enter a NUMBER between 2 and 5!\nTry again!");
+                scanner.next();
+            }
+        }
+        return input;
     }
 }
